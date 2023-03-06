d16b901a8b243fe5946f0eb4d8fb1efd6a983934
Irshal Firdeansyah <irlfdyh@pop-os.localdomain>
2023-03-07 06:54:41 +0700

    initial commit with sample intent

---
+++ app/src/main/java/com/dicoding/playground/intent/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.dicoding.playground.intent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
+    }
+
+}
